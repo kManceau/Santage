a4647d247939d9709c44e71b4754e4e282c58af5
levprost <smolev17@gmail.com>
2024-12-16 12:14:17 +0100

    Child

---
+++ database/migrations/03_create_child_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('children', function (Blueprint $table){
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->string('country');
+            $table->string('address');
+            $table->string('city');
+            $table->string('postal_code');
+            $table->decimal('scolar_note');
+            $table->decimal('behavior_note');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
