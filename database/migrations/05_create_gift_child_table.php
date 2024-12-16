82d3a1802ef4246a43fae79980bc9a8dfebee858
Kevin Manceau <kevin.manceau@gmail.com>
2024-12-16 14:05:09 +0100

    Relations corrected

--- database/migrations/05_create_gift_child_table.php
+++ database/migrations/05_create_gift_child_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('gift_user', function (Blueprint $table) {
+        Schema::create('gift_child', function (Blueprint $table) {
             $table->id();
             $table->foreignId('gift_id')->constrained('gifts');
             $table->foreignId('child_id')->constrained('children');
