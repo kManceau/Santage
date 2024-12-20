c36617a4d8679cec51851e2a3cf760f86f38bc6f
levprost <smolev17@gmail.com>
2024-12-20 15:16:01 +0100

    index and footer responsivity

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,100 +1,38 @@
-<footer class="bg-body-tertiary text-center text-lg-start" style="z-index: 1;">
-  <!-- Grid container -->
-  <div class="container p-4" style="z-index: 1;">
-    <!--Grid row-->
-    <div class="row">
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Links</h5>
 
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" class="text-body">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 4</a>
-          </li>
-        </ul>
+@section('scss')
+@vite(['resources/sass/footer.scss'])
+@endsection
+<footer class="bg-danger text-center text-lg-start">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase" style="color:white;">Links</h5>
+          <ul class="list-unstyled mb-0">
+            <li><a href="#!" style="color:white;text-decoration:none;">About us</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase" style="color:white;">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!" style="color:white;text-decoration:none;">Politique de confidentialité</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase" style="color:white;">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!" style="color:white;text-decoration:none;">Social media Santa</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase" style="color:white;">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!" style="color:white;text-decoration:none;">Donate to Santa</a></li>
+          </ul>
+        </div>
       </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!" class="text-body">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" class="text-body">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!" class="text-body">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-body">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      <!--Grid column-->
     </div>
-    <!--Grid row-->
-  </div>
-  <!-- Grid container -->
-
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2020 Copyright:
-    <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-</footer>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2020 Copyright:
+      <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+  </footer>
